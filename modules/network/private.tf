e8960854cfcc91f48d2aec6d8a6ea11cca22b95d
davigdc <davigdc10@gmail.com>
2025-10-27 21:58:46 -0300

    refactor: terraform module

--- modules/network/private.tf
+++ modules/network/private.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone = "${data.aws_region.current.name}a"
+  availability_zone = "${data.aws_region.current.region}a"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}_subnet_private_1a",
       "kubernetes.io/role/internal-elb" = 1
@@ -16,10 +16,10 @@ resource "aws_subnet" "eks_subnet_private_1a" {
 resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
-  availability_zone = "${data.aws_region.current.name}b"
+  availability_zone = "${data.aws_region.current.region}b"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}_subnet_private_1b",
       "kubernetes.io/role/internal-elb" = 1
--- modules/network/public.tf
+++ modules/network/public.tf
@@ -1,11 +1,11 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 0)
-  availability_zone       = "${data.aws_region.current.name}a"
+  availability_zone       = "${data.aws_region.current.region}a"
   map_public_ip_on_launch = true
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}_subnet_public_1a",
       "kubernetes.io/role/elb" = 1
@@ -17,11 +17,11 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "${data.aws_region.current.name}b"
+  availability_zone       = "${data.aws_region.current.region}b"
   map_public_ip_on_launch = true
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}_subnet_public_1b",
       "kubernetes.io/role/elb" = 1
